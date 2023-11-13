54c745a3e8a77fc6289f046191f2807463defa77
Danya Kovinskiy <129722708+Danilo666228@users.noreply.github.com>
2023-11-13 19:07:04 +0300

    Тема 1 - Кроме 5 задания Тема 2 - Задача 1 - Сделано

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import kotlin.concurrent.thread
+
 //Тема 1: Основные задачи
 //Задача 1
 //fun main() {
@@ -90,3 +92,33 @@
 //        println("Создать нечетное число невозможно")
 //    }
 //}
+//Тема: Массивы.
+//Задача 1.
+fun main() {
+    println("Введите кол-во строк в массиве: ")
+    val rows = readln()!!.toInt()
+
+    println("Введите кол-во столбцов в массиве: ")
+    val cols = readln()!!.toInt()
+
+    val array = Array(rows) { IntArray(cols) }
+
+    println("Введите $rows * $cols трехзначных чисел, разделяя каждое число нажатием Enter: ")
+    for (i in 0 until rows) {
+        for (j in 0 until cols) {
+            array[i][j] = readln()!!.toInt()
+        }
+
+    }
+    val flatArray = array.flatMap { it.toList() }.flatMap { it.toString().toList() }
+    val uniqueDigits = flatArray.distinct().count()
+    println("Двумерный массив: ")
+    for (i in array) {
+        for (j in i) {
+            print("$j\t")
+        }
+    }
+    println("В массиве использовано $uniqueDigits уникальных чисел")
+
+
+}
